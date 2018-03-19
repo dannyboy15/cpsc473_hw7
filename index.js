91c12874247bb045cb015bb9b5ef00f71849f781
Daniel Bravo <bravoappdev@gmail.com>
2018-03-19 00:35:38 -0700

    Add custom error page

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ var server = http.createServer(function(req, res) {
   var filePath = extract(req.url);
   fs.readFile(filePath, function(err, data) {
     if (err) {
-      handleError(err, res);
+      // handleError(err, res);
+      fs.readFile("app/error.html", function(err, data) {
+        if (err) {
+          handleError(err, res);
+          return;
+        } else {
+          res.end(data);
+        }
+      });
       return;
     } else {
       res.end(data);
